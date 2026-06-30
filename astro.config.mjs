91e427890c07f1eb4c3eb902ae90fdf96cf1f7ae
TarikaTheHorse <violette.and.john@gmail.com>
2026-06-29 21:59:23 -0700

    Start site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,32 +4,50 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
+    site: 'http://localhost',
+    integrations: [mdx(), sitemap(), alpinejs()],
+    fonts: [
+        {
+            provider: fontProviders.local(),
+            name: 'Cormorant',
+            cssVariable: '--font-cormorant',
+            fallbacks: ['serif'],
+            options: {
+                variants: [
+                    {
+                        src: ['./src/assets/fonts/Cormorant-Regular.ttf'],
+                        weight: 400,
+                        style: 'normal',
+                        display: 'swap',
+                    },
+                    {
+                        src: ['./src/assets/fonts/Cormorant-Italic.ttf'],
+                        weight: 400,
+                        style: 'italic',
+                        display: 'swap',
+                    },
+                ],
+            },
+        },
+        {
+            provider: fontProviders.local(),
+            name: 'Amarante',
+            cssVariable: '--font-amarante',
+            fallbacks: ['cursive'],
+            options: {
+                variants: [
+                    {
+                        src: ['./src/assets/fonts/Amarante-Regular.ttf'],
+                        weight: 400,
+                        style: 'normal',
+                        display: 'swap',
+                    },
+                ],
+            },
+        },
+    ],
 });
