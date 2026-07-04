b1a345da3052598bad7673f7234935dcce8ea70e
TarikaTheHorse <violette.and.john@gmail.com>
2026-07-03 23:15:30 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'http://localhost',
+    site: 'https://aristocraticarietta.neocities.org',
     integrations: [mdx(), sitemap(),
     alpinejs({
         entrypoint: '/src/alpine'
