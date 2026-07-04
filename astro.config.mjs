6f18fd789032d9e20e3a8cc675491dd3844f43c9
TarikaTheHorse <violette.and.john@gmail.com>
2026-07-03 23:40:21 -0700

    Update links - move stuff to public folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://aristocraticarietta.neocities.org',
+    site: 'https://aristocratic-arietta.neocities.org',
     integrations: [mdx(), sitemap(),
     alpinejs({
         entrypoint: '/src/alpine'
